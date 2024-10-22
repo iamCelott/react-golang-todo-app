b50256117527b8d5f5e04b3ffeb9f59d90eb6d40
iamCelott <dcviriya313@gmail.com>
2024-10-23 00:10:32 +0800

    setup golang for restful API

--- server/main.go
+++ server/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func handleIndex(w http.ResponseWriter, r * http.Request) {
+	fmt.Fprintf(w, "Server Started...")
+}
+
+func main() {
+	mux := http.NewServeMux()
+	var port = "8000"
+
+	mux.HandleFunc("/", handleIndex)
+	
+	fmt.Printf("Server Started at Port %s\n", port)
+	http.ListenAndServe(":"+port, mux)
 }
