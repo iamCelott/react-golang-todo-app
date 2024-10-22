e91f2bfed00e46d643f96602783fc7c2bae80853
iamCelott <dcviriya313@gmail.com>
2024-10-23 00:18:55 +0800

    success connect golang to db

--- server/main.go
+++ server/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func handleIndex(w http.ResponseWriter, r * http.Request) {
@@ -12,9 +15,14 @@ func handleIndex(w http.ResponseWriter, r * http.Request) {
 func main() {
 	mux := http.NewServeMux()
 	var port = "8000"
+	db, err := gorm.Open(mysql.Open("root@tcp(localhost)/db_react_golang_todo_app"))
+	if err != nil {
+		panic("Failed Connect to Database!")
+	}
+	fmt.Println("Success Connect to Database!") 
 
 	mux.HandleFunc("/", handleIndex)
-	
+
 	fmt.Printf("Server Started at Port %s\n", port)
 	http.ListenAndServe(":"+port, mux)
 }
